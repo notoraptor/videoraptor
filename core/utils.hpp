0d517d397e02a961adf0823c65ecd9fdfd9b9f2b
notoraptor <stevenbocco@gmail.com>
2019-05-27 13:36:15 -0400

    [linux] Small updates.

--- core/utils.hpp
+++ core/utils.hpp
@@ -7,23 +7,24 @@
 
 #include "HWDevices.hpp"
 
-#define PIXEL_FMT AV_PIX_FMT_RGBA
 #ifdef WIN32
 #define SEPARATOR '\\'
 #define OTHER_SEPARATOR '/'
-
-inline char* copyString(const char* initialString) {
-	size_t stringLength = strlen(initialString);
-	char* stringCopy = new char[stringLength + 1];
-	memcpy(stringCopy, initialString, stringLength + 1);
-	return stringCopy;
-}
-
-HWDevices* getHardwareDevices();
-
 #else
 #define SEPARATOR '/'
 #define OTHER_SEPARATOR '\\'
 #endif
+
+#define PIXEL_FMT AV_PIX_FMT_RGBA
+
+inline char* copyString(const char* initialString) {
+    size_t stringLength = strlen(initialString);
+    char* stringCopy = new char[stringLength + 1];
+    memcpy(stringCopy, initialString, stringLength + 1);
+    return stringCopy;
+}
+
+HWDevices* getHardwareDevices();
+
 
 #endif //VIDEORAPTORBATCH_UTILS_HPP
