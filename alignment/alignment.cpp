8c6130fd6ec3b61c00acc4f9633f35400ed5f3e2
notoraptor <stevenbocco@gmail.com>
2019-07-25 09:13:18 -0400

    Replace pthreads with OpenMP. Optimize comparison.

--- alignment/alignment.cpp
+++ alignment/alignment.cpp
@@ -5,10 +5,8 @@
 #include <algorithm>
 #include <vector>
 #include <iostream>
-#include <cstring>
-#include <thread>
 #include <cmath>
-#include <sstream>
+#include <omp.h>
 #include "alignment.hpp"
 
 double alignmentScore(std::vector<double>& matrix, const int* a, const int* b, int columns, double interval, int gapScore) {
@@ -60,7 +58,29 @@ inline double pixelSimilarity(const Sequence* p1, int indexP1, const Sequence* p
 			)) / SIMPLE_MAX_PIXEL_DISTANCE;
 }
 
+inline double pixelDistance(const Sequence* p1, int indexP1, const Sequence* p2, int indexP2) {
+	return moderate(
+			std::abs(p1->r[indexP1] - p2->r[indexP2])
+			+ std::abs(p1->g[indexP1] - p2->g[indexP2])
+			+ std::abs(p1->b[indexP1] - p2->b[indexP2]));
+}
+
 #define PIXEL_SIMILARITY(p1, x, y, p2, localX, localY, width) pixelSimilarity(p1, (x) + (y) * (width), p2, (localX) + (localY) * (width))
+#define PIXEL_DISTANCE(p1, x, y, p2, localX, localY, width) pixelDistance(p1, (x) + (y) * (width), p2, (localX) + (localY) * (width))
+
+template <typename T>
+T getMin(T t1, T t2, T t3, T t4, T t5 = -1, T t6 = -1, T t7 = -1, T t8 = -1, T t9 = -1) {
+	T val = t1;
+	if (val > t2) val = t2;
+	if (val > t3) val = t3;
+	if (val > t4) val = t4;
+	if (val > t5) val = t5;
+	if (val > t6) val = t6;
+	if (val > t7) val = t7;
+	if (val > t8) val = t8;
+	if (val > t9) val = t9;
+	return val;
+}
 
 template <typename T>
 T getMax(T t1, T t2, T t3, T t4, T t5 = -1, T t6 = -1, T t7 = -1, T t8 = -1, T t9 = -1) {
@@ -159,7 +179,91 @@ inline double compare(const Sequence* p1, const Sequence* p2, int width, int hei
 	return totalScore / size;
 }
 
+inline double compareFaster(const Sequence* p1, const Sequence* p2, int width, int height, int maximumSimilarityScore) {
+	// x, y:
+	// 0, 0
+	double totalDistance = getMin(
+			PIXEL_DISTANCE(p1, 0 ,0, p2, 0, 0, width),
+			PIXEL_DISTANCE(p1, 0 ,0, p2, 1, 0, width),
+			PIXEL_DISTANCE(p1, 0 ,0, p2, 0, 1, width),
+			PIXEL_DISTANCE(p1, 0 ,0, p2, 1, 1, width));
+	// width - 1, 0
+	totalDistance += getMin(
+			PIXEL_DISTANCE(p1, width - 1, 0, p2, width - 2, 0, width),
+			PIXEL_DISTANCE(p1, width - 1, 0, p2, width - 1, 0, width),
+			PIXEL_DISTANCE(p1, width - 1, 0, p2, width - 2, 1, width),
+			PIXEL_DISTANCE(p1, width - 1, 0, p2, width - 1, 1, width));
+	// 0, height - 1
+	totalDistance += getMin(
+			PIXEL_DISTANCE(p1, 0, height - 1, p2, 0, height - 1, width),
+			PIXEL_DISTANCE(p1, 0, height - 1, p2, 1, height - 1, width),
+			PIXEL_DISTANCE(p1, 0, height - 1, p2, 0, height - 2, width),
+			PIXEL_DISTANCE(p1, 0, height - 1, p2, 1, height - 2, width));
+	// width - 1, height - 1
+	totalDistance += getMin(
+			PIXEL_DISTANCE(p1, width - 1, height - 1, p2, width - 2, height - 1, width),
+			PIXEL_DISTANCE(p1, width - 1, height - 1, p2, width - 1, height - 1, width),
+			PIXEL_DISTANCE(p1, width - 1, height - 1, p2, width - 2, height - 2, width),
+			PIXEL_DISTANCE(p1, width - 1, height - 1, p2, width - 1, height - 2, width));
+	// x, 0
+	for (int x = 1; x <= width - 2; ++x) {
+		totalDistance += getMin(
+				PIXEL_DISTANCE(p1, x, 0, p2, x - 1, 0, width),
+				PIXEL_DISTANCE(p1, x, 0, p2, x, 0, width),
+				PIXEL_DISTANCE(p1, x, 0, p2, x + 1, 0, width),
+				PIXEL_DISTANCE(p1, x, 0, p2, x - 1, 1, width),
+				PIXEL_DISTANCE(p1, x, 0, p2, x, 1, width),
+				PIXEL_DISTANCE(p1, x, 0, p2, x + 1, 1, width));
+	}
+	// x, height - 1
+	for (int x = 1; x <= width - 2; ++x) {
+		totalDistance += getMin(
+				PIXEL_DISTANCE(p1, x, height - 1, p2, x - 1, height - 1, width),
+				PIXEL_DISTANCE(p1, x, height - 1, p2, x, height - 1, width),
+				PIXEL_DISTANCE(p1, x, height - 1, p2, x + 1, height - 1, width),
+				PIXEL_DISTANCE(p1, x, height - 1, p2, x - 1, height - 2, width),
+				PIXEL_DISTANCE(p1, x, height - 1, p2, x, height - 2, width),
+				PIXEL_DISTANCE(p1, x, height - 1, p2, x + 1, height - 2, width));
+	}
+	for (int y = 1; y <= height - 2; ++y) {
+		// 0, y
+		totalDistance += getMin(
+				PIXEL_DISTANCE(p1, 0, y, p2, 0, y - 1, width),
+				PIXEL_DISTANCE(p1, 0, y, p2, 1, y - 1, width),
+				PIXEL_DISTANCE(p1, 0, y, p2, 0, y, width),
+				PIXEL_DISTANCE(p1, 0, y, p2, 1, y, width),
+				PIXEL_DISTANCE(p1, 0, y, p2, 0, y + 1, width),
+				PIXEL_DISTANCE(p1, 0, y, p2, 1, y + 1, width));
+		// width - 1, y
+		totalDistance += getMin(
+				PIXEL_DISTANCE(p1, width - 1, y, p2, width - 2, y - 1, width),
+				PIXEL_DISTANCE(p1, width - 1, y, p2, width - 1, y - 1, width),
+				PIXEL_DISTANCE(p1, width - 1, y, p2, width - 2, y, width),
+				PIXEL_DISTANCE(p1, width - 1, y, p2, width - 1, y, width),
+				PIXEL_DISTANCE(p1, width - 1, y, p2, width - 2, y + 1, width),
+				PIXEL_DISTANCE(p1, width - 1, y, p2, width - 1, y + 1, width));
+	}
+	// x in [1; width - 2], y in [1; height - 2]
+	int remainingSize = (width - 2) * (height - 2);
+	for (int index = 0; index < remainingSize; ++index) {
+		int x = index % (width - 2) + 1;
+		int y = index / (width - 2) + 1;
+		totalDistance += getMin(
+				PIXEL_DISTANCE(p1, x, y, p2, x - 1, y - 1, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x, y - 1, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x + 1, y - 1, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x - 1, y, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x, y, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x + 1, y, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x - 1, y + 1, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x, y + 1, width),
+				PIXEL_DISTANCE(p1, x, y, p2, x + 1, y + 1, width));
+	}
+	return (maximumSimilarityScore - totalDistance) / maximumSimilarityScore;
+}
+
 void sub(Sequence** sequences, int width, int height, int size, double similarityLimit, int i, int jFrom, int jTo) {
+	#pragma omp parallel for default(none) shared(sequences, i, jFrom, jTo, width, height, size, similarityLimit)
 	for (int j = jFrom; j < jTo; ++j) {
 		double score = compare(sequences[i], sequences[j], width, height, size);
 		if (score >= similarityLimit) {
@@ -171,23 +275,18 @@ void sub(Sequence** sequences, int width, int height, int size, double similarit
 
 void classifySimilarities(Sequence** sequences, int nbSequences, int width, int height, double similarityLimit, int v) {
 	std::cout << "STARTING" << std::endl;
-	int size = width * height;
-	int nbThreads = 4;
+	int maximumSimilarityScore = SIMPLE_MAX_PIXEL_DISTANCE * width * height;
 	for (int i = 0; i < nbSequences - 1; ++i) {
-		int a = i + 1;
-		int l = (nbSequences - i - 1) / nbThreads;
-		std::vector<std::thread> processes(nbThreads);
-		for (int t = 0; t < nbThreads - 1; ++t) {
-			processes[t] = std::thread(sub, sequences, width, height, size, similarityLimit, i, a + t * l, a + (t + 1) * l);
+		#pragma omp parallel for default(none) shared(sequences, i, nbSequences, width, height, similarityLimit, maximumSimilarityScore)
+		for (int j = i + 1; j < nbSequences; ++j) {
+			double score = compareFaster(sequences[i], sequences[j], width, height, maximumSimilarityScore);
+			if (score >= similarityLimit) {
+				sequences[j]->classification = sequences[i]->classification;
+				sequences[j]->score = score;
+			}
 		}
-		processes[nbThreads - 1] = std::thread(
-				sub, sequences, width, height, size, similarityLimit, i, a + (nbThreads - 1) * l, nbSequences);
-		for (auto& process: processes)
-			process.join();
 		if ((i + 1) % 100 == 0) {
-			std::cout
-				<< "(*) Image " << i + 1 << " / " << nbSequences
-				<< " (" << l << " per thread on " << nbThreads << " threads)" << std::endl;
+			std::cout << "(*) Image " << i + 1 << " / " << nbSequences << std::endl;
 		}
 	}
 }
